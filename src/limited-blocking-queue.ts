3ebda729ad89ea5b51884b334730921700615910
j.neubauer <j.neubauer@quadient.com>
2022-05-10 10:39:30 +0200

    Implemented close() - will reject all pending pull/push promises.

--- src/limited-blocking-queue.ts
+++ src/limited-blocking-queue.ts
@@ -1,11 +1,15 @@
 export class LimitedBlockingQueue {
 
+    private closed: boolean;
     private readonly limit: number;
     private readonly queue: any[];
-    private readonly notEmptyResolves: ((_:any)=>any)[];
-    private readonly notFullResolves: ((_:any)=>any)[];
+    // list of resolve,reject tuples.
+    private readonly notEmptyResolves: ([(_:any)=>any, (_:any)=>any])[];
+    // list of resolve,reject tuples.
+    private readonly notFullResolves: ([(_:any)=>any, (_:any)=>any])[];
 
     constructor(size = 1) {
+        this.closed = false;
         this.queue = [];
         this.limit = size;
         this.notEmptyResolves = [];
@@ -15,16 +19,19 @@ export class LimitedBlockingQueue {
     // Asynchronous. Returns promise that will be satisfied when the item is really pushed.
     // It could be not immediate, when the buffer is full.
     push(item: any): Promise<any> {
+        if(this.closed) {
+            return Promise.reject(new Error("Queue is closed"));
+        }
         if (this.notEmptyResolves.length) {
             // assert(this.queue.length === 0)
             // Don't put the item to queue - give it directly to the waiting
             // consumer through the resolve of promise he obtained.
-            this.notEmptyResolves.shift()!(item);
+            this.notEmptyResolves.shift()![0](item);
             return Promise.resolve();
         } else {
             if (this.queue.length === this.limit) {
-                return new Promise((resolve) => {
-                    this.notFullResolves.push(resolve);
+                return new Promise((resolve, reject) => {
+                    this.notFullResolves.push([resolve, reject]);
                 }).then(() => {
                     this.queue.push(item);
                 });
@@ -37,17 +44,27 @@ export class LimitedBlockingQueue {
 
     // Asynchronous. Returns promise that will be resolved with the object pulled from the queue.
     pull() {
+        if(this.closed) {
+            return Promise.reject(new Error("Queue is closed"));
+        }
         if (this.queue.length === 0) {
-            return new Promise((resolve) => {
-                this.notEmptyResolves.push(resolve);
+            return new Promise((resolve, reject) => {
+                this.notEmptyResolves.push([resolve, reject]);
             });
         } else {
             let item = this.queue.shift();
             if (this.notFullResolves.length) {
-                this.notFullResolves.shift()!(undefined);
+                this.notFullResolves.shift()![0](undefined);
             }
             return Promise.resolve(item);
         }
+    }
+    
+    close() {
+        this.closed = true;
+        // reject waiting promises - both the waiting pull and push operations
+        this.notFullResolves.forEach(([_,reject]) => {reject(new Error("Queue is closed"))});
+        this.notEmptyResolves.forEach(([_,reject]) => {reject(new Error("Queue is closed"))});
     }
 
     get length() {
--- tests/limited-blocking-queue.test.ts
+++ tests/limited-blocking-queue.test.ts
@@ -17,3 +17,23 @@ test('pull before push', async () => {
     await promise1;
     await promise2;
 });
+
+test('close will reject all waiting pull operations', async () => {
+    let q = new LimitedBlockingQueue(2);
+    let pullPromise1 = q.pull();
+    let pullPromise2 = q.pull();
+    q.close();
+    await expect(pullPromise1).rejects.toThrow('Queue is closed');
+    await expect(pullPromise2).rejects.toThrow('Queue is closed');
+});
+
+test('close will reject all waiting push operations', async () => {
+    let q = new LimitedBlockingQueue(1);
+    await q.push(1);
+    let pushPromise1 = q.push(2);
+    let pushPromise2 = q.push(2);
+    q.close();
+    await expect(pushPromise1).rejects.toThrow('Queue is closed');
+    await expect(pushPromise2).rejects.toThrow('Queue is closed');
+});
+
