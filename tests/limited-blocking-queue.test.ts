5738a4a6d96c8866a6937baaa2122814566ca63f
j.neubauer <j.neubauer@quadient.com>
2022-05-10 10:49:27 +0200

    next version

--- tests/limited-blocking-queue.test.ts
+++ tests/limited-blocking-queue.test.ts
@@ -20,20 +20,30 @@ test('pull before push', async () => {
 
 test('close will reject all waiting pull operations', async () => {
     let q = new LimitedBlockingQueue(2);
+
+    // test pull before close will be rejected after close
     let pullPromise1 = q.pull();
     let pullPromise2 = q.pull();
     q.close();
     await expect(pullPromise1).rejects.toThrow('Queue is closed');
     await expect(pullPromise2).rejects.toThrow('Queue is closed');
+    
+    // test pull after close
+    await expect(q.pull()).rejects.toThrow('Queue is closed');
 });
 
 test('close will reject all waiting push operations', async () => {
     let q = new LimitedBlockingQueue(1);
     await q.push(1);
+    
+    // test push before close will be rejected after close
     let pushPromise1 = q.push(2);
-    let pushPromise2 = q.push(2);
+    let pushPromise2 = q.push(3);
     q.close();
     await expect(pushPromise1).rejects.toThrow('Queue is closed');
     await expect(pushPromise2).rejects.toThrow('Queue is closed');
+    
+    // test push after close
+    await expect(q.push(4)).rejects.toThrow('Queue is closed');
 });
 
